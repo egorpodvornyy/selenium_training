6c2cd9c7b24b5d024469e118030bc5eeb155f3cb
egorpodvornyy <egorpodvornyy@gmail.com>
2021-11-23 15:47:32 +0600

    Задание 7

--- selenium-java-maven/src/test/java/ru/selenium/raif/maven/MyFirstTest.java
+++ selenium-java-maven/src/test/java/ru/selenium/raif/maven/MyFirstTest.java
@@ -7,8 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -26,16 +31,16 @@ public class MyFirstTest {
 
     @Test
     public void testSearch() {
-        driver.get("http://www.youtube.com");
-        driver.findElement(By.name("search_query")).click();
-        driver.findElement(By.name("search_query")).sendKeys("Алексей Баранцев" + Keys.ENTER);
-        driver.findElement(By.id("channel-title")).click();
-        wait.until(titleIs("Alexei Barantsev - YouTube"));
+        driver.get("https://software-testing.ru/lms/mod/lesson/view.php?id=232674&pageid=8823");
+        driver.findElement(By.name("username")).sendKeys("egorpodvornyy");
+        driver.findElement(By.name("password")).sendKeys("wawawawa1");
+        driver.findElement(By.cssSelector("input[value=Вход]")).click();
+
     }
 
-    @After
-    public void stop() {
-        driver.quit();
-        driver = null;
-    }
+//    @After
+//    public void stop() {
+//        driver.quit();
+//        driver = null;
+//    }
 }
---
+++ selenium-java-maven/src/test/java/ru/selenium/raif/maven/PresentPagesTest.java
@@ -0,0 +1,54 @@
+package ru.selenium.raif.maven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class PresentPagesTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private int amountElements;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 1);
+    }
+
+    @Test
+    public void testSearch() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+
+        amountElements = driver.findElements(By.cssSelector("td#sidebar li#app-")).size();
+
+        for(int i = 0; i < amountElements; i++) {
+            driver.findElement(By.xpath("(//td[@id='sidebar'] //li[@id='app-'])[" + (i+1) + "]")).click();
+
+            if(driver.findElement(By.xpath("(//td[@id='sidebar'] //li[@id='app-'])[" + (i+1) + "]")).findElements(By.cssSelector("li")).size() > 0) {
+                for(int j = 0; j < driver.findElement(By.xpath("(//td[@id='sidebar'] //li[@id='app-'])[" + (i+1) + "]")).findElements(By.cssSelector("li")).size(); j++) {
+                    driver.findElement(By.xpath("(//td[@id='sidebar'] //li[@id='app-'])[" + (i+1) + "] //li[" + (j+1) + "]")).click();
+                }
+            }
+
+            wait.until((WebDriver d) -> d.findElement(By.cssSelector("h1")));
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
